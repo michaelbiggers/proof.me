e69531e2e088add74099d3150344230598c6fbb7
michaelbiggers <mbiggers@knights.ucf.edu>
2015-04-17 10:47:03 -0400

    Most up-to-date
    Current working branch; will contain some bugs

--- app.js
+++ app.js
@@ -1,51 +1,64 @@
-var app = angular.module('flapperNews', ['ui.router'])
-app.config([
-	'$stateProvider',
-	'$urlRouterProvider',
-	function ($stateProvider, $urlRouterProvider){
-		$stateProvider
-			.state('home', {
-				url:'/home',
-				templateUrl: '/home.html',
-				controller: 'MainCtrl'
-			});
-		$urlRouterProvider.otherwise('home');
-	}]);
-app.factory('posts', [function(){
+angular.module('proofMe', ['ui.router'])
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider,$urlRouterProvider) {
+
+	$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: '/home.html',
+			controller: 'MainCtrl'
+		})
+
+		.state('posts', {
+			url:'/posts/{id}',
+			templateUrl: '/posts.html',
+			controller: 'PostsCtrl'
+		});
+
+	$urlRouterProvider.otherwise('home');
+
+}])
+.factory('posts',[function(){
 	var o = {
 		posts: []
 	};
 	return o;
-}]);
+}])
+.controller('MainCtrl', [
+'$scope',
+'posts',
+function($scope,posts){
+  $scope.test = 'Hello world!';
 
-app.controller('MainCtrl', [
+  $scope.posts = posts.posts;
+  
+
+  $scope.addPost = function(){
+  	if ($scope.title === '') {return;}
+  	$scope.posts.push({
+  		title:$scope.title,
+  		link: $scope.link,
+  		upvotes:0,
+  		comments:[
+  			{author: 'Joe', body: 'Cool post', upvotes: 0},
+  			{author: 'Kim', body: 'Sweet post', upvotes: 0}
+  		]
+  	});
+  	$scope.title ='';
+  	$scope.link ='';
+  }
+
+  $scope.incrementUpvotes = function(post) {
+  	post.upvotes += 1;
+  }
+}])
+.controller('PostsCtrl',[
 	'$scope',
-	'$posts',
-	function($scope, posts){
-
-		$scope.posts = posts.posts;
-
-		$scope.posts = [
-  			{title: 'post 1', upvotes: 5},
-  			{title: 'post 2', upvotes: 2},
-			{title: 'post 3', upvotes:15},
-			{title: 'post 4', upvotes: 9},
-			{title: 'post 5', upvotes: 4}
-		];
-
-		$scope.addPost = function(){
-			if(!$scope.title || $scope.title === '') {return;}
-
-			$scope.posts.push({
-				title: $scope.title,
-				link: $scope.link,
-				upvotes:0
-			});
-			$scope.title ='';
-			$scope.link = '';
-		};
-
-		$scope.incrementUpvotes = function(post) {
-			post.upvotes +=1;
-		};
+	'$stateParams',
+	'posts',
+	function ($scope,$stateParams,posts){
+		$scope.post = post.posts[$stateParams.id];
 	}]);
+
